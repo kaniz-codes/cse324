da4a7aee2104e4b3b44ca0953442a5a9e707e7bc
kaniz-codes <social.kaniz@gmail.com>
2026-06-20 23:39:11 +0600

    Task 4: Identify comment or not

---
+++ lab-mid/task4.cpp
@@ -0,0 +1,23 @@
+// Write a C/C++ program to identify whether a given line is a comment or not.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string a;
+    cout <<"Enter a string: "<<endl;
+    getline(cin, a);
+
+    int n = a.length();
+
+    if((a[0] == '/' && a[1] == '*') && (a[n-1] == '/' && a[n-2] == '*')){
+        cout << "This is a comment." <<endl;
+    }
+    else if((a[0] == '/' && a[1]== '/')){
+        cout << "This is a comment." <<endl;
+    }
+    else{
+        cout<<"This is not a comment."<<endl;
+    }
+
+}
