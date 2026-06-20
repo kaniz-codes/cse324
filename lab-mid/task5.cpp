e461b81abc0fa9c94f71f0db7ce70cac88fbb8a1
kaniz-codes <social.kaniz@gmail.com>
2026-06-21 00:03:59 +0600

    Task 5: Identify valid keywords

---
+++ lab-mid/task5.cpp
@@ -0,0 +1,28 @@
+// Write a C/C++ program to identify valid keywords.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string word;
+
+    string keywords[] = {
+        "int", "float", "char", "double", "if", "else", 
+        "for", "while", "do", "return", "break", "continue", 
+        "switch", "case", "void", "class", "struct"
+    };
+    int n = sizeof(keywords) / sizeof(keywords[0]);
+
+    cout << "Enter a word: ";
+    cin >> word;
+
+    for (int i = 0; i < n; i++) {
+        if (word == keywords[i]) {
+            cout << "This is a Keyword.";
+        }
+    }
+    cout << "This is not a Keyword.";
+
+}
+
